33ee5474b880e8efaf8a723b7f0bd4e9b4a34253
Wilson <wilsonsun@arizona.edu>
2025-11-01 23:57:35 +0000

    db view

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from .routers import diary
 from . import models
-from .database import engine
+from .database import get_db, engine
 
 # 自动创建表
 models.Base.metadata.create_all(bind=engine)
@@ -14,3 +15,7 @@ app.include_router(diary.router)
 @app.get("/")
 def root():
     return {"message": "Justus backend is running"}
+
+@app.get("/debug/diaries")
+def get_diaries(db: Session = Depends(get_db)):
+    return db.query(models.Diary).all()
